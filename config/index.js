4876b38b9222bfcf377241768d3ccdfdc2479241
oc <2336990515@qq.com>
2018-08-16 15:31:34 +0800

    modify build path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vuepack/dist/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
